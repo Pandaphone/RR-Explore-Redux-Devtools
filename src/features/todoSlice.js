aa2dba0565fda12e16f1fa3109e3fdbb4b408113
Pandaphone <shawngreene075@gmail.com>
2022-08-30 13:06:41 -0400

    Did some work on the slice. Initizlized a Todo Component

--- src/features/todoSlice.js
+++ src/features/todoSlice.js
@@ -1,31 +1,25 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-    items: []
+    toDoItems: []
 }
 
 export const todoSlice = createSlice({
-    name: 'todos',
+    name: 'todo',
     initialState,
     reducers: {
-        addTodo: (state, action) => {
-            return { items: [...state.items, action.payload]}
+        addToDo: (state, action) => {
+            return { toDoItems: [...state.toDoItems, action.payload]
+        }},
+        removeTodo: (state) => {
+            return { value: state.value - 1 }
         },
-        removeOne: (state, action) => {
-            console.log(action)
-            let array = [...state.items]
-            let index = action.payload
-            if(index !== -1) {
-                array.splice(index, 1)
-                return { items: array }
-            }
-        },
-        clearTodo: () => {
-            return { items: [] }
+        clearToDos: () => {
+            return { toDoItems: [] }
         }
     }
 })
 
-export const { addTodo, removeOne, clearTodo } = todoSlice.actions
+export const { addToDo, removeTodo, clearToDos } = todoSlice.actions
 
 export default todoSlice.reducer
